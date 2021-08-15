646f8270cdc1dc9458291e6617845bed79c627a7
123452123-codr <81916900+123452123-codr@users.noreply.github.com>
2021-08-15 12:07:17 +0530

    abc

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-window.onload = function(){
-    firebase.database().ref('Info').on("value", function(snapshot){
+(function(){
+    var database = firebase.database();
+    database.ref('Info').on("value", (snapshot) => {
         var a1 = snapshot.val().q1;
         var a2 = snapshot.val().q2;
         var a3 = snapshot.val().q3;
@@ -10,164 +11,231 @@ window.onload = function(){
         var a8 = snapshot.val().q8;
         var a9 = snapshot.val().q9;
         var a10 = snapshot.val().q10;
-    });
-};
-
-(function(){
-    // Functions
-    function buildQuiz(){
-      // variable to store the HTML output
-      const output = [];
-  
-      // for each question...
-      myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-  
-          // variable to store the list of possible answers
-          const answers = [];
-  
-          // and for each available answer...
-          for(letter in currentQuestion.answers){
-  
-            // ...add an HTML radio button
-            answers.push(
-              `<label>
-                <input type="radio" name="question${questionNumber}" value="${letter}">
-                ${letter} :
-                ${currentQuestion.answers[letter]}
-              </label>`
+        // Functions
+        function buildQuiz(){
+        // variable to store the HTML output
+        const output = [];
+    
+        // for each question...
+        myQuestions.forEach(
+          (currentQuestion, questionNumber) => {
+    
+            // variable to store the list of possible answers
+            const answers = [];
+    
+            // and for each available answer...
+            for(letter in currentQuestion.answers){
+    
+              // ...add an HTML radio button
+              answers.push(
+                `<label>
+                  <input type="radio" name="question${questionNumber}" value="${letter}">
+                  ${letter} :
+                  ${currentQuestion.answers[letter]}
+                </label>`
+              );
+            }
+    
+            // add this question and its answers to the output
+            output.push(
+              `<div class="slide">
+                <div class="question"> ${currentQuestion.question} </div>
+                <div class="answers"> ${answers.join("")} </div>
+              </div>`
             );
           }
-  
-          // add this question and its answers to the output
-          output.push(
-            `<div class="slide">
-              <div class="question"> ${currentQuestion.question} </div>
-              <div class="answers"> ${answers.join("")} </div>
-            </div>`
-          );
+        );
+    
+        // finally combine our output list into one string of HTML and put it on the page
+        quizContainer.innerHTML = output.join('');
+      }
+    
+      function showResults(){
+    
+        // gather answer containers from our quiz
+        const answerContainers = quizContainer.querySelectorAll('.answers');
+    
+        // keep track of user's answers
+        let numCorrect = 0;
+    
+        // for each question...
+        myQuestions.forEach( (currentQuestion, questionNumber) => {
+    
+          // find selected answer
+          const answerContainer = answerContainers[questionNumber];
+          const selector = `input[name=question${questionNumber}]:checked`;
+          const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+    
+          // if answer is correct
+          if(userAnswer === currentQuestion.correctAnswer){
+            // add to the number of correct answers
+            numCorrect++;
+    
+            // color the answers green
+            answerContainers[questionNumber].style.color = 'lightgreen';
+          }
+          // if answer is wrong or blank
+          else{
+            // color the answers red
+            answerContainers[questionNumber].style.color = 'red';
+          }
+        });
+    
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+      }
+    
+      function showSlide(n) {
+        slides[currentSlide].classList.remove('active-slide');
+        slides[n].classList.add('active-slide');
+        currentSlide = n;
+        if(currentSlide === 0){
+          previousButton.style.display = 'none';
         }
-      );
-  
-      // finally combine our output list into one string of HTML and put it on the page
-      quizContainer.innerHTML = output.join('');
-    }
-  
-    function showResults(){
-  
-      // gather answer containers from our quiz
-      const answerContainers = quizContainer.querySelectorAll('.answers');
-  
-      // keep track of user's answers
-      let numCorrect = 0;
-  
-      // for each question...
-      myQuestions.forEach( (currentQuestion, questionNumber) => {
-  
-        // find selected answer
-        const answerContainer = answerContainers[questionNumber];
-        const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-  
-        // if answer is correct
-        if(userAnswer === currentQuestion.correctAnswer){
-          // add to the number of correct answers
-          numCorrect++;
-  
-          // color the answers green
-          answerContainers[questionNumber].style.color = 'lightgreen';
-        }
-        // if answer is wrong or blank
         else{
-          // color the answers red
-          answerContainers[questionNumber].style.color = 'red';
+          previousButton.style.display = 'inline-block';
         }
-      });
-  
-      // show number of correct answers out of total
-      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-    }
-  
-    function showSlide(n) {
-      slides[currentSlide].classList.remove('active-slide');
-      slides[n].classList.add('active-slide');
-      currentSlide = n;
-      if(currentSlide === 0){
-        previousButton.style.display = 'none';
+        if(currentSlide === slides.length-1){
+          nextButton.style.display = 'none';
+          submitButton.style.display = 'inline-block';
+        }
+        else{
+          nextButton.style.display = 'inline-block';
+          submitButton.style.display = 'none';
+        }
       }
-      else{
-        previousButton.style.display = 'inline-block';
+    
+      function showNextSlide() {
+        showSlide(currentSlide + 1);
       }
-      if(currentSlide === slides.length-1){
-        nextButton.style.display = 'none';
-        submitButton.style.display = 'inline-block';
+    
+      function showPreviousSlide() {
+        showSlide(currentSlide - 1);
       }
-      else{
-        nextButton.style.display = 'inline-block';
-        submitButton.style.display = 'none';
-      }
-    }
-  
-    function showNextSlide() {
-      showSlide(currentSlide + 1);
-    }
-  
-    function showPreviousSlide() {
-      showSlide(currentSlide - 1);
-    }
-  
-    // Variables
-    const quizContainer = document.getElementById('quiz');
-    const resultsContainer = document.getElementById('results');
-    const submitButton = document.getElementById('submit');
-    const myQuestions = [
-      {
-        question: "How many gold medals have India won in olympics till now?",
-        answers: {
-          a: "8",
-          b: "9",
-          c: "10",
-          d: "11"
+    
+      // Variables
+      const quizContainer = document.getElementById('quiz');
+      const resultsContainer = document.getElementById('results');
+      const submitButton = document.getElementById('submit');
+      const myQuestions = [
+        {
+          question: "How many gold medals have India won in olympics till now?",
+          answers: {
+            a: "8",
+            b: "9",
+            c: "10",
+            d: "11"
+          },
+          correctAnswer: a1
         },
-        correctAnswer: a1
-      },
-      {
-        question: "In which year was softball introduced to the Olympics?",
-        answers: {
-          a: "2008",
-          b: "2016",
-          c: "1947",
-          d: "1996"
+        {
+          question: " In which year was softball introduced to the Olympics?",
+          answers: {
+            a: "2008",
+            b: "2016",
+            c: "1947",
+            d: "1996"
+          },
+          correctAnswer: a2
         },
-        correctAnswer: a2
-      },
-      {
-        question: "Which tool can you use to ensure code quality?",
-        answers: {
-          a: "Angular",
-          b: "jQuery",
-          c: "RequireJS",
-          d: "ESLint"
+        {
+          question: "In which country did the Olympics originate?",
+          answers: {
+            a: "India",
+            b: "Greece",
+            c: "Pakistan",
+            d: "U.S."
+          },
+          correctAnswer: a3
         },
-        correctAnswer: "d"
-      }
-    ];
-  
-    // Kick things off
-    buildQuiz();
-  
-    // Pagination
-    const previousButton = document.getElementById("previous");
-    const nextButton = document.getElementById("next");
-    const slides = document.querySelectorAll(".slide");
-    let currentSlide = 0;
-  
-    // Show the first slide
-    showSlide(currentSlide);
-  
-    // Event listeners
-    submitButton.addEventListener('click', showResults);
-    previousButton.addEventListener("click", showPreviousSlide);
-    nextButton.addEventListener("click", showNextSlide);
-  })();
+        {
+            question: "In which country did the Olympics originate?",
+            answers: {
+              a: "India",
+              b: "Greece",
+              c: "Pakistan",
+              d: "U.S."
+            },
+            correctAnswer: a3
+          },
+          {
+            question: "In which country did the Olympics originate?",
+            answers: {
+              a: "India",
+              b: "Greece",
+              c: "Pakistan",
+              d: "U.S."
+            },
+            correctAnswer: a3
+          },
+          {
+            question: "In which country did the Olympics originate?",
+            answers: {
+              a: "India",
+              b: "Greece",
+              c: "Pakistan",
+              d: "U.S."
+            },
+            correctAnswer: a3
+          },
+          {
+            question: "In which country did the Olympics originate?",
+            answers: {
+              a: "India",
+              b: "Greece",
+              c: "Pakistan",
+              d: "U.S."
+            },
+            correctAnswer: a3
+          },
+          {
+            question: "In which country did the Olympics originate?",
+            answers: {
+              a: "India",
+              b: "Greece",
+              c: "Pakistan",
+              d: "U.S."
+            },
+            correctAnswer: a3
+          },
+          {
+            question: "In which country did the Olympics originate?",
+            answers: {
+              a: "India",
+              b: "Greece",
+              c: "Pakistan",
+              d: "U.S."
+            },
+            correctAnswer: a3
+          },
+          {
+            question: "In which country did the Olympics originate?",
+            answers: {
+              a: "India",
+              b: "Greece",
+              c: "Pakistan",
+              d: "U.S."
+            },
+            correctAnswer: a3
+          }
+      ];
+    
+      // Kick things off
+      buildQuiz();
+    
+      // Pagination
+      const previousButton = document.getElementById("previous");
+      const nextButton = document.getElementById("next");
+      const slides = document.querySelectorAll(".slide");
+      let currentSlide = 0;
+    
+      // Show the first slide
+      showSlide(currentSlide);
+    
+      // Event listeners
+      submitButton.addEventListener('click', showResults);
+      previousButton.addEventListener("click", showPreviousSlide);
+      nextButton.addEventListener("click", showNextSlide);
+    });
+})();
   
