2d28ab22692af091cb9b823b036dfe0d18cabfc1
Rajanjor <81916900+123452123-codr@users.noreply.github.com>
2021-08-16 14:16:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,8 @@ var date = document.getElementById('date').text;
 var next = document.getElementById('submitDetails');
 
 next.addEventListener("click", function(){
-    if(fname == null && mclass == null && date == null || fname != null && mclass == null && date == null || fname == null && mclass != null && date == null || fname == null && mclass == null && date != null || fname != null && mclass != null && date == null || fname == null && mclass != null && date != null || fname != null && mclass == null && date != null)
-    {
-        alert("Please fill out all the fields.");
-    }
-    else
-    {
-        const text = {Name: fname, ClassOpted: mclass, SubmitDate: date};
-        var database = firebase.database();
-        database.ref('Info').set(text);
-        window.open("quiz.html","_self");
-    }
+    const text = {Name: fname, ClassOpted: mclass, SubmitDate: date};
+    const store = JSON.stringify(text);
+    localStorage.setItem("Store", store);
+    window.open("quiz.html","_self");
 });
